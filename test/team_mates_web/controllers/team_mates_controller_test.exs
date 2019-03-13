2b62579883a4ea9a1b0ab53d938ac1c2741a3f70
Rabea Gleissner <rabeagleissner@gmail.com>
2019-03-13 10:05:28 +0800

    Add test for creating a user

--- test/team_mates_web/controllers/team_mates_controller_test.exs
+++ test/team_mates_web/controllers/team_mates_controller_test.exs
@@ -1,11 +1,23 @@
 defmodule TeammatesWeb.TeammatesControllerTest do
+  use ExUnit.Case, async: true
   use TeammatesWeb.ConnCase
   import Teammates.Factory
+  alias Teammates.TeamMember
+  alias Teammates.Repo
+
+  test "creates team member" do
+    conn = post(build_conn(), "/api/teammates", [name: "Fred", timeZone: "US"])
+    all_team_members = TeamMember.all()
+
+    assert conn.status == 200
+    assert length(all_team_members) == 1
+    assert all_team_members[0]["name"] == "Fred"
+  end
 
   test "gets all users" do
     conn = build_conn()
     fred = insert(:team_member)
-    response = get(conn, Routes.teammates_path(conn, :index))
+    response = get(conn, "/api/teammates")
 
     assert response.status == 200
     assert json_response(response, 200) == [%{
